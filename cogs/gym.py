f363fe683e8b18a82d6612d6a6dc6092e16a0a35
Zach <eckert.zach95@gmail.com>
2018-12-13 00:12:00 -0500

    Restructure leader management commands

--- cogs/gym.py
+++ cogs/gym.py
@@ -10,54 +10,64 @@ class Gym():
         self.gymleader = json.loads(open("gymleaders.json").read()) if isfile("gymleaders.json") else {}
         self.elite = json.loads(open("elitefour.json").read()) if isfile("elitefour.json") else []
 
-
     @commands.group(pass_context=True)
-    async def list(self,ctx):
+    async def leader(self,ctx):
         if ctx.invoked_subcommand is None:
-            await self.bot.send_message(ctx.message.channel,"~list <type>\nTypes:\nleaders\nelitefour")
+            await self.bot.send_message(ctx.message.channel,"You need a subcommand for this to work! Please try again")
 
-    @list.command(pass_context=True)
-    async def elitefour(self,ctx):
-        url1 = "http://static.tumblr.com/8ead6fd4ef321fc779d824ec3d39f5cd/9vi46my/6uso1uc3y/tumblr_static_515l7v2awykgk0sgcwow4wgog.png"
-        for userid in self.elite:
-            user = ctx.message.server.get_member(userid)
-            em = discord.Embed(name="Elite Four",description="Elite Four")
-            em.set_thumbnail(url=url1)
-            em.add_field(name="Discord Username",value=user.mention,inline=True)
-            await self.bot.send_message(ctx.message.channel,embed=em)
+    @leader.command(pass_context=True)
+    async def list(self,ctx,ltype : str):
+        #leader list <ltype>
+        if ltype.replace(" ","")[:3].lower() == "gym":
 
-    @list.command(pass_context=True)
-    async def leaders(self,ctx):
-        url1 = "https://pokemongo.gamepress.gg/sites/pokemongo/files/2018-02/Badge_GymLeader_GOLD_01.png"
-        for userid in self.gymleader.keys():
-            user = ctx.message.server.get_member(userid)
-            em = discord.Embed(name="Gym Leader", description="Gym Leader")
-            em.set_thumbnail(url=url1)
-            em.add_field(name="Discord Username",value=user.mention,inline=True)
-            em.add_field(name="Badge Title",value=self.gymleader[userid]['badgeName'],inline=True)
-            em.add_field(name="Challenge Description",value=self.gymleader[userid]['desc'],inline=True)
-            await self.bot.send_message(ctx.message.channel,embed=em)
+            url1 = "https://pokemongo.gamepress.gg/sites/pokemongo/files/2018-02/Badge_GymLeader_GOLD_01.png"
+            for userid in self.gymleader.keys():
+                user = ctx.message.server.get_member(userid)
+                em = discord.Embed(name="Gym Leader", description="Gym Leader")
+                em.set_thumbnail(url=url1)
+                em.add_field(name="Discord Username",value=user.mention,inline=True)
+                em.add_field(name="Badge Title",value=self.gymleader[userid]['badgeName'],inline=True)
+                em.add_field(name="Challenge Description",value=self.gymleader[userid]['desc'],inline=True)
+                await self.bot.send_message(ctx.message.channel,embed=em)
+        elif ltype.replace(" ","")[:9].lower() == "elitefour":
 
-    @commands.group(pass_context=True)
-    async def add(self,ctx):
-        if ctx.invoked_subcommand is None:
-            await self.bot.say("~add <type>\nTypes include:\nleader\nelitefour")
+            url1 = "http://static.tumblr.com/8ead6fd4ef321fc779d824ec3d39f5cd/9vi46my/6uso1uc3y/tumblr_static_515l7v2awykgk0sgcwow4wgog.png"
+            for userid in self.elite:
+                user = ctx.message.server.get_member(userid)
+                em = discord.Embed(name="Elite Four",description="Elite Four")
+                em.set_thumbnail(url=url1)
+                em.add_field(name="Discord Username",value=user.mention,inline=True)
+                await self.bot.send_message(ctx.message.channel,embed=em)
+        else:
+            await self.bot.sent_message(ctx.message.channel,"I'm not sure I got that. Please try again")
 
-    @add.command(pass_context=True)
-    async def leader(self,ctx,user : discord.Member, desc : str, badgeName : str):
-        """Adds a gym leader"""
-        self.gymleader[user.id] = {}
-        self.gymleader[user.id]['desc'] = desc
-        self.gymleader[user.id]['badgeName'] = badgeName
-        await self.bot.send_message(ctx.message.channel,"Gym Leader added:\n{}\n{}\n{}".format(user.mention,self.gymleader[user.id]['desc'],self.gymleader[user.id]['badgeName']))
-        open("gymleaders.json",'w').write(json.dumps(self.gymleader))
+    @leader.command(pass_context=True)
+    async def add(self,ctx,ltype : str,user : discord.Member,desc : str = None,badgeName : str = None):
+        if ltype.replace(" ","")[:3].lower() == "gym":
+            #Adds a gym leader
+            self.gymleader[user.id] = {}
+            self.gymleader[user.id]['desc'] = desc
+            self.gymleader[user.id]['badgeName'] = badgeName
+            await self.bot.send_message(ctx.message.channel,"Gym Leader added:\n{}\n{}\n{}".format(user.mention,self.gymleader[user.id]['desc'],self.gymleader[user.id]['badgeName']))
+            open("gymleaders.json",'w').write(json.dumps(self.gymleader))
+        elif ltype.replace(" ","")[:9].lower() == "elitefour":
+            #Adds an Elite Four Member
+            self.elite.append(user.id)
+            await self.bot.send_message(ctx.message.channel,"Elite Four Added:\n{}".format(user.mention))
+            open("elitefour.json",'w').write(json.dumps(self.elite))
+        else:
+            await self.bot.sent_message(ctx.message.channel,"I'm not sure I got that. Please try again")
 
-    @add.command(pass_context=True)
-    async def elite_four(self,ctx,user : discord.Member):
-        """Adds an Elite Four Member"""
-        self.elite.append(user.id)
-        await self.bot.send_message(ctx.message.channel,"Elite Four Added:\n{}".format(user.mention))
-        open("elitefour.json",'w').write(json.dumps(self.elite))
+    @leader.command(pass_context=True)
+    async def remove(self,ctx,ltype : str,user : discord.Member):
+        if ltype.replace(" ","")[:3].lower() == "gym":
+            del self.gymleader[user.id]
+            await self.bot.send_message(ctx.message.channel,"Gym Leader removed: {}".format(user.mention))
+        elif ltype.replace(" ","")[:9].lower() == "elitefour":
+            del self.elite[user.id]
+            await self.bot.send_message(ctx.message.channel,"Elite Four Member removed: {}".format(user.mention))
+        else:
+            await self.bot.send_message(ctx.message.channel,"I'm not sure I got that. Please try again")
 
 def setup(bot):
     bot.add_cog(Gym(bot))
