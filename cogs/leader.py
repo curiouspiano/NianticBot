0750d319cde065ce3110e1dda030401aed14c4cf
Zach <eckert.zach95@gmail.com>
2018-12-14 04:43:49 -0500

    Implemented '~leader add gym' using the database

--- cogs/leader.py
+++ cogs/leader.py
@@ -2,19 +2,14 @@ import discord
 from discord.ext import commands
 import json
 from os.path import isfile
-from lib.simplemysql import SimpleMysql as sql
+import calendar
+import datetime
 
 class Leader():
     def __init__(self, bot):
         self.bot= bot
         self.gymleader = json.loads(open("gymleaders.json").read()) if isfile("gymleaders.json") else {}
         self.elite = json.loads(open("elitefour.json").read()) if isfile("elitefour.json") else []
-        db = sql(
-                host='127.0.0.1',
-                db='bfr',
-                user='bot',
-                passwd='BotPass3234!',
-                keep_alive=True)
 
     @commands.group(pass_context=True)
     async def leader(self,ctx):
@@ -48,19 +43,38 @@ class Leader():
             await self.bot.sent_message(ctx.message.channel,"I'm not sure I got that. Please try again")
 
     @leader.command(pass_context=True)
-    async def add(self,ctx,ltype : str,user : discord.Member,desc : str = None,badgeName : str = None):
+    async def add(self,ctx,ltype : str,user : discord.Member,desc : str = None,badgeName : str = None,nickname : str = None,trainerName : str = None,badgeMonth : str = calendar.month_name[(datetime.datetime.today().month+1 if datetime.datetime.today().month < 12 else 1)],badgeYear : int = datetime.datetime.today().year):
+        badgeMonthNum = list(calendar.month_name).index(badgeMonth)
         if ltype.replace(" ","")[:3].lower() == "gym":
+            await self.bot.SQL.connect()
+            await self.bot.SQL.query("\
+                            REPLACE INTO users\
+                            SET id={},nick=\"{}\",trainerName=\"{}\";".format(\
+                            user.id,nickname,trainerName))
+            cursor = await self.bot.SQL.query("SELECT max(id) FROM badges")
+            badgeid = (await self.bot.SQL.fetch_all_list(cursor,'max(id)'))[0] + 1
+            await self.bot.SQL.query("\
+                    REPLACE INTO badges\
+                    SET id={},\
+                        description=\"{}\",\
+                        name=\"{}\",\
+                        start_available=\"{}-{}-01\",\
+                        end_available=\"{}-{}-{}\";".format(\
+                        badgeid,desc,badgeName,\
+                        badgeYear,badgeMonthNum,\
+                        badgeYear,badgeMonthNum,calendar.monthrange(badgeYear,badgeMonthNum)[1]))
+            self.bot.SQL.disconnect()
             #Adds a gym leader
-            self.gymleader[user.id] = {}
-            self.gymleader[user.id]['desc'] = desc
-            self.gymleader[user.id]['badgeName'] = badgeName
+            #self.gymleader[user.id] = {}
+            #self.gymleader[user.id]['desc'] = desc
+            #self.gymleader[user.id]['badgeName'] = badgeName
             await self.bot.send_message(ctx.message.channel,"Gym Leader added:\n{}\n{}\n{}".format(user.mention,self.gymleader[user.id]['desc'],self.gymleader[user.id]['badgeName']))
-            open("gymleaders.json",'w').write(json.dumps(self.gymleader))
-        elif ltype.replace(" ","")[:9].lower() == "elitefour":
-            #Adds an Elite Four Member
-            self.elite.append(user.id)
-            await self.bot.send_message(ctx.message.channel,"Elite Four Added:\n{}".format(user.mention))
-            open("elitefour.json",'w').write(json.dumps(self.elite))
+            #open("gymleaders.json",'w').write(json.dumps(self.gymleader))
+        #elif ltype.replace(" ","")[:9].lower() == "elitefour":
+            ##Adds an Elite Four Member
+            #self.elite.append(user.id)
+            #await self.bot.send_message(ctx.message.channel,"Elite Four Added:\n{}".format(user.mention))
+            #open("elitefour.json",'w').write(json.dumps(self.elite))
         else:
             await self.bot.sent_message(ctx.message.channel,"I'm not sure I got that. Please try again")
 
