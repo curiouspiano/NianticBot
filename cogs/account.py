e215caa3963b9cc140122aa1f61ed9c231253a29
Mike <mikewfitch1997@gmail.com>
2018-12-16 21:13:58 -0500

    Updated account.py to commit date and nickname to the database on user join.

--- cogs/account.py
+++ cogs/account.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+from datetime import date
 
 class Account:
     def __init__(self, bot):
@@ -11,13 +12,14 @@ class Account:
         discID = user.id
         trainerName = None
         trainerCode = None
+        nick = user.display_name
         try:
             await self.bot.SQL.connect()
             result = await self.bot.SQL.query("SELECT id FROM users WHERE id={}".format(int(user.id)))
             if result.rowcount > 0:
                 await self.bot.say("You're alread registered... xxx")
+                self.bot.SQL.disconnect()
                 return
-
             await self.bot.say("Alright, let's get you ready to fight!")
             await self.bot.say("First, I need your PokemonGO trainer name:")
             resp = await self.bot.wait_for_message(author=user, timeout=180)
@@ -25,11 +27,11 @@ class Account:
             await self.bot.say("Alright, now I need your friend code, please input it in one string <000000000000>")
             resp = await self.bot.wait_for_message(author=user, timeout=180)
             trainerCode = resp.content
-            sqlStr = "INSERT INTO users(id, trainerName, friendCode) VALUES ({}, '{}', '{}')".format(int(user.id), trainerName, trainerCode)
-            print(sqlStr)
+            nick = user.name
+            sqlStr = "INSERT INTO users(id, nick, trainerName, friendCode, joinDate) VALUES ({}, '{}', '{}', '{}', '{}')".format(int(user.id), nick, trainerName, trainerCode, date.today())
             await self.bot.SQL.query(sqlStr)
-            await self.bot.say("Welcome {} to the League!\n".format(user.name))
-            await self.bot.SQL.disconnect()
+            await self.bot.say("Welcome {} to the League!\n".format(nick))
+            self.bot.SQL.disconnect()
         except Exception as e:
             await self.bot.say("There was an issue... please ensure you're not adding spaces to your responses and try again")
             print(e)
