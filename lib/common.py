1e20ca14b565f615c66be563cd583bce3d1640a3
Mike <mikewfitch1997@gmail.com>
2019-01-10 22:14:13 -0500

    Completed the make_selection utility. Wide use among user choices.

--- lib/common.py
+++ lib/common.py
@@ -2,6 +2,7 @@ import requests
 import asyncio
 from lib.mysql import *
 
+INDEX_TO_EMOJI = ["1⃣", "2⃣", "3⃣", "4⃣", "5⃣", "6⃣", "7⃣", "8⃣", "9⃣"]
 #function loads our database with all of the existing pokemon, their ID/Name/shiny sprite
 async def fetch_poke_data(bot):
     await bot.SQL.connect()
@@ -24,5 +25,32 @@ async def fetch_poke_data(bot):
             print(e)
     bot.SQL.disconnect()
 
+
+
 async def make_selection(bot, ctx, options):
-    print("ey man make yo stuff here")
+    #uni function to let a user make a decision based on an array of options
+    #bot - bot object, used for interacting
+    #ctx - context object that invoked the command
+    #options - array of strings, returns the value, not index
+    
+    msg = "Please react with your choice\n"
+    for i in range(len(options)):
+        msg += "{} - {}\n".format(INDEX_TO_EMOJI[i], options[i])
+
+    priMessage = await bot.send_message(ctx.message.channel, msg)
+
+    reactionOptions = []
+
+    for i in range(len(options)):
+        reactionOptions.append(INDEX_TO_EMOJI[i])
+        await bot.add_reaction(priMessage, INDEX_TO_EMOJI[i])
+
+    choice = await bot.wait_for_reaction(emoji=reactionOptions, user=ctx.message.author, message=priMessage, timeout=180)
+    
+    answer = choice.reaction.emoji
+
+    final = options[reactionOptions.index(answer)]
+
+    await bot.delete_message(priMessage)
+
+    return final
