70081cc5fdf51d544e6839f4f719776c259a2efb
Mike <mikewfitch1997@gmail.com>
2019-08-26 23:03:36 -0400

    Updated with a new and improved, really cool AUTOMATIC TEAM AUTHENTICATOR HOLY SHIT IT'S REAL.
    hardcoded channel numbers & rank Ids.
    
    Will pull out to configuration eventually, maybe.

--- cogs/shrug.py
+++ cogs/shrug.py
@@ -4,7 +4,8 @@ from discord.ext import commands
 class Shrug(commands.Cog):
     def __init__(self, bot):
         self.bot= bot
-
+    
+    @commands.Cog.listener()
     async def on_message(self, message):
         if(self.bot.user.mentioned_in(message) and message.author != self.bot.user and not message.mention_everyone):
             await self.bot.send_message(message.channel, '¯\_(ツ)_/¯')
